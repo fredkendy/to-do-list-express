d21fb26b7252afea6667e6cea5f9d1fe33ef2740
fredkendy <fredkendy9@gmail.com>
2023-05-10 10:09:04 -0300

    Usando middlewares

--- app.js
+++ app.js
@@ -3,12 +3,33 @@ const express = require('express');
 //Criando instância do app e chamando express
 const app = express();
 
+//Usando middlewares: verificando se existe algo json sendo passado
+app.use(express.json())
+//Passado esse parametro no body da requisção json do postman:
+// {
+//     "task": {
+//         "title": "x",
+//         "done": false
+//     }
+// }
+
+//Criando função exemplo para testar middlewares
+const log = (req, res, next) => {
+    console.log(req.body)
+    console.log(`Data: ${Date.now()}`)
+    next()  //passa execução pro próximo middleware
+}
+
+//Chamando função log com middlewares
+app.use(log)
+
 //Criou rota e devolvendo html
 app.get('/', (req, res) => {
     res.send('<h1>Minha lista de tarefas</h1>')
 })
 
 app.get('/json', (req, res) => {    //qdo chamar /json, cai aqui (ex: localhost:3000/json)
+    console.log(req.body)
     res.json({title: 'Tarefa X', done: true})    //devolver no formato json
 })
 
